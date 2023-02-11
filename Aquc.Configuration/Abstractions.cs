61d94b441ef539a694f0c05aaabd7c34a9e4db41
aquamarine5 <50990424+aquamarine5@users.noreply.github.com>
2023-02-11 16:10:16 +0800

    test

---
+++ Aquc.Configuration/Abstractions.cs
@@ -0,0 +1,22 @@
+﻿namespace Aquc.Configuration.Abstractions;
+
+public interface IConfigurationStruct
+{
+    public IConfigurationManifest ConfigurationManifest { get; set; }
+}
+public interface IConfigurationManifest
+{
+    public int version { get; set; }
+}
+public interface IConfigurationFlow<T>:IDisposable where T : IConfigurationStruct
+{
+    public IConfigurationSource<T> Source { get; set; }
+    public T Data { get; }
+    public void Save();
+}
+public interface IConfigurationSource<T> where T : IConfigurationStruct
+{
+    public IConfigurationFlow<T> GetFlow();
+    public T Data { get; set; }
+    public Task SaveAsync();
+}
---
+++ Aquc.Configuration/Config.cs
@@ -0,0 +1,101 @@
+﻿using Aquc.Configuration.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Aquc.Configuration;
+
+public class ConfigurationDefaultManifest : IConfigurationManifest
+{
+    public ConfigurationDefaultManifest(int ver) => version = ver;
+    public int version { get; set; }
+}
+public class ConfigurationBuilder<T> where T:IConfigurationStruct
+{
+    public string? filePath;
+    public T? defaultValue;
+    public T? currentValue;
+
+    private ConfigurationBuilder() { }
+
+    public static ConfigurationBuilder<T> Create() =>new ();
+    public virtual async Task<ConfigurationBuilder<T>> BindJsonAsync(string fileName)
+    {
+        filePath = fileName;
+        if (File.Exists(fileName))
+        {
+            var file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            currentValue = await JsonSerializer.DeserializeAsync<T>(file);
+        }
+        else
+        {
+            var file = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite);
+            await WriteDefaultValue(file);
+            currentValue = defaultValue;
+        }
+        return this;
+    }
+    protected virtual async Task WriteDefaultValue(FileStream file)
+    {
+        if (defaultValue == null)
+            throw new NullReferenceException("Should call SetDefault() first when the configuration file is not existed.");
+        await JsonSerializer.SerializeAsync(file, defaultValue);
+    }
+    public virtual ConfigurationBuilder<T> SetDefault(Func<T> func) 
+    {
+        defaultValue=func.Invoke();
+        return this;
+    }
+    public virtual ConfigurationBuilder<T> SetDefault(T defaultValue)
+    {
+        this.defaultValue = defaultValue;
+        return this;
+    }
+    public virtual IConfigurationSource<T> Build()
+    {
+        if (filePath == null)
+            throw new NullReferenceException("Should call BindJson() first.");
+        if (currentValue == null)
+            throw new NullReferenceException("Should call BindJson() first.");
+        return new ConfigurationDefaultSource<T>(filePath, currentValue);
+    }
+}
+
+public class ConfigurationDefaultSource<T> : IConfigurationSource<T> where T:IConfigurationStruct
+{
+    protected string filePath;
+    public ConfigurationDefaultSource(string filePath, T data) => (Data, this.filePath) = (data, filePath);
+    public T Data { get; set; }
+
+    public async Task SaveAsync()
+    {
+        using var file = new FileStream(filePath, FileMode.Open, FileAccess.Write);
+        await JsonSerializer.SerializeAsync(file, Data);
+    }
+
+    public IConfigurationFlow<T> GetFlow()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class ConfigurationDefaultFlow<T> : IConfigurationFlow<T> where T : IConfigurationStruct
+{
+    public T Data => Source.Data;
+    public ConfigurationDefaultFlow(IConfigurationSource<T> source) => Source = source;
+    public IConfigurationSource<T> Source { get; set; }
+
+    public void Dispose()
+    {
+        Save();
+        GC.SuppressFinalize(this);
+    }
+
+    public void Save()
+    {
+        Source.SaveAsync();
+    }
+}
